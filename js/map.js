df0a782fb940a8cd3b457994e0c9108fea16addc
Mohamed Sididque <mmohamedsiddique@hawk.iit.edu>
2018-11-20 16:16:56 -0600

    Update map.js

--- js/map.js
+++ js/map.js
@@ -5,22 +5,7 @@ function initMap(){
   
   var map = new google.maps.Map(
     document.getElementById('map'), {
-    zoom: 13, center: bridge
+    zoom: 13, center: {lat: 35.714436, lng: 139.729721},
     }
   );
-
-  var marker1 = new google.maps.Marker({
-    position: tokyo,
-    map: map
-  });
-        
-  var marker2 = new google.maps.Marker({
-    position: shinjuku,
-    map: map
-  });
-        
-  var marker3 = new google.maps.Marker({
-    position: iidabashistation,
-    map: map
-  });
 }
